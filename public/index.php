488bc75a783eeed6b72ef5016358488f9caf984c
yvillavicencioi <76493193+yvillavicencioi@users.noreply.github.com>
2021-01-01 21:54:28 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -41,3 +41,12 @@ $rid = "In4LANe-bbAAAAAAAAAAAA==";
 echo $col->deleteDocument($rid);
 
 ?>
+<!doctype html>
+<html>
+<head>
+  <title>Prueba de Bootstrap 4</title> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
